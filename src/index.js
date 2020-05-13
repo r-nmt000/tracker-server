63f8c57093658f96774be54268a1d7b6293497d8
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-14 00:15:45 +0900

    feat: add body-parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoUri = require('../env');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 mongoose.connect(mongoUri, {
