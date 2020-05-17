7f75117f660f50c43cf4710d85635c166b63a969
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-17 22:33:44 +0900

    feat: add track model and api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoUri = require('../env');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
